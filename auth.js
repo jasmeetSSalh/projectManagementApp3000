64e5cbc9c0c638ade1713d22713e95365af20d88
Gavin Dhaliwal <gavindhaliwal123@gmail.com>
2023-12-18 18:13:55 -0500

    Rebasing

--- auth.js
+++ auth.js
@@ -1,29 +1,29 @@
 // import axios from "axios";
 
-// const BASE_ENDPOINT = "http://localhost:3000";
-// const loginButton = document.getElementById("login-button");
-// const signupButton = document.getElementById("signup-button");
-// const signUpForm = document.getElementById("signup-form");
-// const loginForm = document.getElementById("login-form");
+const BASE_ENDPOINT = "http://localhost:3000";
+const loginButton = document.getElementById("login-button");
+const signupButton = document.getElementById("signup-button");
+const signUpForm = document.getElementById("signup-form");
+const loginForm = document.getElementById("login-form");
 
-// loginButton.addEventListener("click", async (e) => {
-//   const loginFormData = new FormData(loginForm);
-//   //   for (let pair of loginFormData.entries()) {
-//   //     console.log(pair[0] + ": " + pair[1]);
-//   //   }
+loginButton.addEventListener("click", async (e) => {
+  const loginFormData = new FormData(loginForm);
+  //   for (let pair of loginFormData.entries()) {
+  //     console.log(pair[0] + ": " + pair[1]);
+  //   }
 
-//   /*
-// make a post request to the users/login endpoint, which compares the password with the chosen user
-// */
-//   const response = await fetch(`${BASE_ENDPOINT}/users/login`, {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application.json",
-//     },
-//     body: JSON.stringify(loginFormData),
-//   });
-//   console.log(response);
-// });
+  /*
+make a post request to the users/login endpoint, which compares the password with the chosen user
+*/
+  const response = await fetch(`${BASE_ENDPOINT}/users/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(loginFormData),
+  });
+  console.log(response);
+});
 
 // signupButton.addEventListener("click", async (e) => {
 //   const signUpFormData = new FormData(signUpForm);
