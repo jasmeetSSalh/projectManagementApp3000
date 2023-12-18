61d9f7201fb98066f10d18021a2f50f589612081
Gavin Dhaliwal <gavindhaliwal123@gmail.com>
2023-12-17 23:09:23 -0500

    Created basic auth using bcrypt, created login and user endpoints

--- index.js
+++ index.js
@@ -1,42 +1,102 @@
-import Task from './task.js';
+import Task from "./task.js";
 
-import express from 'express';
-import bodyParser from 'body-parser';
+import express from "express";
+import bodyParser from "body-parser";
 // import pg from 'pg'; // for later
-import axios from 'axios';
-
+import axios from "axios";
+import bcrypt, { hash } from "bcrypt";
 
 const app = express();
 const port = 3000;
 
-app.set("view engine", "ejs"); 
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"))
+app.use(express.static("public"));
 
+// For having post requests to the users
+app.use(express.json());
 
 // store all the tasks in this array
 const globalTaskArray = [];
 
 // task cases so we don't have to test using the database
-const task1 = new Task(1, "Task 1", "This is task 1", "John", "2021-01-01", "Not Started", "2021-01-01", "John");
-const task2 = new Task(2, "Task 2", "This is task 2", "John", "2021-01-01", "In Progress", "2021-01-01", "John");
-const task3 = new Task(3, "Task 3", "This is task 3", "John", "2021-01-01", "Completed", "2021-01-01", "John");
+const task1 = new Task(
+  1,
+  "Task 1",
+  "This is task 1",
+  "John",
+  "2021-01-01",
+  "Not Started",
+  "2021-01-01",
+  "John"
+);
+const task2 = new Task(
+  2,
+  "Task 2",
+  "This is task 2",
+  "John",
+  "2021-01-01",
+  "In Progress",
+  "2021-01-01",
+  "John"
+);
+const task3 = new Task(
+  3,
+  "Task 3",
+  "This is task 3",
+  "John",
+  "2021-01-01",
+  "Completed",
+  "2021-01-01",
+  "John"
+);
 globalTaskArray.push(task1);
 globalTaskArray.push(task2);
 globalTaskArray.push(task3);
 
 //Default route
 app.get("/", async (req, res) => {
-  res.render("index.ejs",{
-    globalTaskArray: globalTaskArray
+  res.render("index.ejs", {
+    globalTaskArray: globalTaskArray,
   });
 });
 
+// auth
+const users = [];
+app.get("/users", (req, res) => {
+  res.json(users);
+});
 
+app.post("/users", async (req, res) => {
+  try {
+    const hashedPass = await bcrypt.hash(req.body.password, 10); // salt and hash
+    console.log(hashedPass);
+    const user = { name: req.body.name, password: hashedPass }; // store the hashed password in our database
+    users.push(user); // for short term storage
+    res.status(201).send(); // send empty response to client
+  } catch {
+    res.status(500).send();
+  }
+});
+
+app.post("/users/login", async (req, res) => {
+  const user = users.find((user) => user.name === req.body.name);
+  if (user == null) {
+    return res.status(400).send("Cannot find user");
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.send("Success");
+    } else {
+      res.send("Invalid password");
+    }
+  } catch {
+    res.status(500).send();
+  }
+});
 
 //To start the server
 //run by typing "node index.js" in terminal or install nodemon and run by typing "nodemon index.js"
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
-  });
-
+  console.log(`Server is running at http://localhost:${port}`);
+});
