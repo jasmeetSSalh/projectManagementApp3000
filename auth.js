e8463ead8c809ce3025b54c4cba83b93b506d782
Gavin Dhaliwal <gavindhaliwal123@gmail.com>
2023-12-18 10:51:55 -0500

    Created basic HTML signup and login pages, with auth.js to handle login requests

---
+++ auth.js
@@ -0,0 +1,6 @@
+const loginButton = document.getElementById("login-button");
+const signupButton = document.getElementById("signup-button");
+const signUpForm = document.getElementById("signup-form");
+const loginForm = document.getElementById("loginForm");
+
+loginButton.addEventListener("submit");
