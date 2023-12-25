b50c68bf9faf848ff46ed3506a3daba9c3f9ca8c
jasmeetSSalh <jasmeetsalh4@gmail.com>
2023-12-25 13:13:34 -0500

    Updated tasks definition

--- task.js
+++ task.js
@@ -1,8 +1,9 @@
 export default class Task {
-    constructor(id, name, description, assignedTo, dueDate, status, lastModified, creator) {
+    constructor(id, name, description, personName, assignedTo, dueDate, status, lastModified, creator) {
         this.id = id;
         this.name = name;
         this.description = description;
+        this.personName = personName;
         this.assignedTo = assignedTo;
         this.dueDate = dueDate;
         this.status = status;
