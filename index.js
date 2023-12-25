d8411538d6bcb123b97550273e89aeea95a65574
irfanjey <133928024+irfanjey@users.noreply.github.com>
2023-12-25 13:11:18 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -17,55 +17,47 @@ app.use(express.static("public"));
 app.use(express.json());
 
 // store all the tasks in this array
+const projects = [];
 const globalTaskArray = [];
+const globalTaskArray2 = [];
 
 // task cases so we don't have to test using the database
-const task1 = new Task(
-  1,
-  "Task 1",
-  "This is task 1",
-  "John",
-  "2021-01-01",
-  "Not Started",
-  "2021-01-01",
-  "John"
-);
-const task2 = new Task(
-  2,
-  "Task 2",
-  "This is task 2",
-  "John",
-  "2021-01-01",
-  "In Progress",
-  "2021-01-01",
-  "John"
-);
-const task3 = new Task(
-  3,
-  "Task 3",
-  "This is task 3",
-  "John",
-  "2021-01-01",
-  "Completed",
-  "2021-01-01",
-  "John"
-);
-const task4 = new Task(
-  4,
-  "Task 4",
-  "This is task 4",
-  "John",
-  "2021-01-01",
-  "Not Started",
-  "2021-01-01",
-  "John"
-);
+const task1 = new Task(1, "Task 1", "This is task 1", 'Irfan', '2023-12-03', '2023-12-06', "Not Started", "2021-01-01", "John");
+const task2 = new Task(2, "Task 2", "This is task 2", 'Jasmeet', '2023-12-06', '2023-12-09', "In Progress", "2021-01-01", "John");
+const task3 = new Task(3, "Task 3", "This is task 3", 'Gavin', '2023-12-09', '2023-12-20', "Completed", "2021-01-01", "John");
+const task4 = new Task(4, "Task 4", "This is task 4", 'Sharyar', '2023-12-12', '2023-12-24', "Not Started", "2021-01-01", "John");
 globalTaskArray.push(task1);
 globalTaskArray.push(task2);
 globalTaskArray.push(task3);
 globalTaskArray.push(task4);
 
+const task5 = new Task(1, "Task 5", "This is task 5", 'John', '2023-12-03', '2023-12-12', "Not Started", "2021-01-01", "John");
+const task6 = new Task(2, "Task 6", "This is task 6", 'Bob', '2023-12-06', '2023-12-07', "In Progress", "2021-01-01", "John");
+const task7 = new Task(3, "Task 7", "This is task 7", 'Obama', '2023-12-09', '2023-12-20', "Completed", "2021-01-01", "John");
+const task8 = new Task(4, "Task 8", "This is task 8", 'Joeover', '2023-12-20', '2023-12-28', "Completed", "2021-01-01", "John");
+
+globalTaskArray2.push(task5);
+globalTaskArray2.push(task6);
+globalTaskArray2.push(task7);
+globalTaskArray2.push(task8);
+
+projects.push(globalTaskArray);
+projects.push(globalTaskArray2);
+
+
 //Default route
+// app.get("/timeline", async (req, res) => {
+//   res.render("test.ejs",{
+//     globalTasksArray: globalTaskArray
+//   });
+// });
+
+app.get("/timeline", async (req, res) => {
+  res.render("test.ejs",{
+    projects: projects
+  });
+});
+
 app.get("/", async (req, res) => {
   res.render("index.ejs", {
     globalTaskArray: globalTaskArray,
@@ -87,51 +79,51 @@ app.get("/users/signup", (req, res) => {
 });
 
 // Custom user endpoints, might be used to display specific user data
-app.get("/users/:id", (req, res) => {
-  res.redirect("/");
-});
+// app.get("/users/:id", (req, res) => {
+//   res.redirect("/");
+// });
 
-// Can incorporate password requirements here
-app.post("/users/signup", async (req, res) => {
-  if (req.body.password == req.body.confirmPassword) {
-    try {
-      const hashedPass = await bcrypt.hash(req.body.password, 10); // salt and hash
-      // console.log(hashedPass);
-      // How will roles be assigned?
-      const user = new Person(
-        req.body.name,
-        req.body.role,
-        req.body.email,
-        hashedPass,
-        users.length
-      );
-      // console.log(user);
-      users.push(user); // for short term storage
-      res.status(201).redirect(`/users/${user.id}`);
-    } catch {
-      res.status(500).send("An error has occurred, please try again");
-    }
-  } else {
-    res.status(401).send(new Error("Non matching passwords"));
-  }
-});
+// // Can incorporate password requirements here
+// app.post("/users/signup", async (req, res) => {
+//   if (req.body.password == req.body.confirmPassword) {
+//     try {
+//       const hashedPass = await bcrypt.hash(req.body.password, 10); // salt and hash
+//       // console.log(hashedPass);
+//       // How will roles be assigned?
+//       const user = new Person(
+//         req.body.name,
+//         req.body.role,
+//         req.body.email,
+//         hashedPass,
+//         users.length
+//       );
+//       // console.log(user);
+//       users.push(user); // for short term storage
+//       res.status(201).redirect(`/users/${user.id}`);
+//     } catch {
+//       res.status(500).send("An error has occurred, please try again");
+//     }
+//   } else {
+//     res.status(401).send(new Error("Non matching passwords"));
+//   }
+// });
 
-app.post("/users/login", async (req, res) => {
-  const user = users.find((user) => user.email === req.body.email);
-  if (user == null) {
-    return res.status(400).send("Cannot find user");
-  }
-  try {
-    if (await bcrypt.compare(req.body.password, user.password)) {
-      res.redirect("/");
-    } else {
-      res.send("Invalid password");
+// app.post("/users/login", async (req, res) => {
+//   const user = users.find((user) => user.email === req.body.email);
+//   if (user == null) {
+//     return res.status(400).send("Cannot find user");
+//   }
+//   try {
+//     if (await bcrypt.compare(req.body.password, user.password)) {
+//       res.redirect("/");
+//     } else {
+//       res.send("Invalid password");
       
-    }
-  } catch {
-    res.status(500).send("An error has occurred, please try again");
-  }
-});
+//     }
+//   } catch {
+//     res.status(500).send("An error has occurred, please try again");
+//   }
+// });
 
 //To start the server
 //run by typing "node index.js" in terminal or install nodemon and run by typing "nodemon index.js"
