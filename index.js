df8cd1440bcdfd366aacc6821f5795f3a5dafa3a
irfanjey <133928024+irfanjey@users.noreply.github.com>
2023-12-27 22:56:22 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ globalTaskArray.push(task4);
 const task5 = new Task(1, "Task 5", "This is task 5", 'John', '2023-12-03', '2023-12-12', "Not Started", "2021-01-01", "John");
 const task6 = new Task(2, "Task 6", "This is task 6", 'Bob', '2023-12-06', '2023-12-07', "In Progress", "2021-01-01", "John");
 const task7 = new Task(3, "Task 7", "This is task 7", 'Obama', '2023-12-09', '2023-12-20', "Completed", "2021-01-01", "John");
-const task8 = new Task(4, "Task 8", "This is task 8", 'Joeover', '2023-12-20', '2023-12-28', "Completed", "2021-01-01", "John");
+const task8 = new Task(4, "Task 8", "This is task 8", 'Joever', '2023-12-20', '2023-12-28', "Completed", "2021-01-01", "John");
 
 globalTaskArray2.push(task5);
 globalTaskArray2.push(task6);
@@ -44,16 +44,8 @@ globalTaskArray2.push(task8);
 projects.push(globalTaskArray);
 projects.push(globalTaskArray2);
 
-
-//Default route
-// app.get("/timeline", async (req, res) => {
-//   res.render("test.ejs",{
-//     globalTasksArray: globalTaskArray
-//   });
-// });
-
 app.get("/timeline", async (req, res) => {
-  res.render("test.ejs",{
+  res.render("timeline.ejs",{
     projects: projects
   });
 });
@@ -79,51 +71,51 @@ app.get("/users/signup", (req, res) => {
 });
 
 // Custom user endpoints, might be used to display specific user data
-// app.get("/users/:id", (req, res) => {
-//   res.redirect("/");
-// });
+app.get("/users/:id", (req, res) => {
+  res.redirect("/");
+});
 
-// // Can incorporate password requirements here
-// app.post("/users/signup", async (req, res) => {
-//   if (req.body.password == req.body.confirmPassword) {
-//     try {
-//       const hashedPass = await bcrypt.hash(req.body.password, 10); // salt and hash
-//       // console.log(hashedPass);
-//       // How will roles be assigned?
-//       const user = new Person(
-//         req.body.name,
-//         req.body.role,
-//         req.body.email,
-//         hashedPass,
-//         users.length
-//       );
-//       // console.log(user);
-//       users.push(user); // for short term storage
-//       res.status(201).redirect(`/users/${user.id}`);
-//     } catch {
-//       res.status(500).send("An error has occurred, please try again");
-//     }
-//   } else {
-//     res.status(401).send(new Error("Non matching passwords"));
-//   }
-// });
+// Can incorporate password requirements here
+app.post("/users/signup", async (req, res) => {
+  if (req.body.password == req.body.confirmPassword) {
+    try {
+      const hashedPass = await bcrypt.hash(req.body.password, 10); // salt and hash
+      // console.log(hashedPass);
+      // How will roles be assigned?
+      const user = new Person(
+        req.body.name,
+        req.body.role,
+        req.body.email,
+        hashedPass,
+        users.length
+      );
+      // console.log(user);
+      users.push(user); // for short term storage
+      res.status(201).redirect(`/users/${user.id}`);
+    } catch {
+      res.status(500).send("An error has occurred, please try again");
+    }
+  } else {
+    res.status(401).send(new Error("Non matching passwords"));
+  }
+});
 
-// app.post("/users/login", async (req, res) => {
-//   const user = users.find((user) => user.email === req.body.email);
-//   if (user == null) {
-//     return res.status(400).send("Cannot find user");
-//   }
-//   try {
-//     if (await bcrypt.compare(req.body.password, user.password)) {
-//       res.redirect("/");
-//     } else {
-//       res.send("Invalid password");
+app.post("/users/login", async (req, res) => {
+  const user = users.find((user) => user.email === req.body.email);
+  if (user == null) {
+    return res.status(400).send("Cannot find user");
+  }
+  try {
+    if (await bcrypt.compare(req.body.password, user.password)) {
+      res.redirect("/");
+    } else {
+      res.send("Invalid password");
       
-//     }
-//   } catch {
-//     res.status(500).send("An error has occurred, please try again");
-//   }
-// });
+    }
+  } catch {
+    res.status(500).send("An error has occurred, please try again");
+  }
+});
 
 //To start the server
 //run by typing "node index.js" in terminal or install nodemon and run by typing "nodemon index.js"
